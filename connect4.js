0211e4499d6d88d905eeae87ab34d41a05ba7c7f
AlphaWolf7335 <joni.decoster@student.arteveldehs.be>
2024-04-18 15:55:07 +0200

    made function setGame .js

--- connect4.js
+++ connect4.js
@@ -12,3 +12,20 @@ const currColums = [];
 window.onload = function() {
     setGame();
 };
+
+function setGame() {
+    board = []; 
+    columns = [5,5,5,5,5,5,5]
+    for (let r = 0; r < rows; r++) {
+        let row = [];
+        for (let c = 0; c < columns; c++) {
+            row.push(' ');
+            let tile = document.createElement("div");
+            tile.id = r.toString() + "-" + c.toString();
+            tile.classList.add("tile");
+            tile.addEventListener("click", setPiece);
+            document.getElementById("board").append(tile);
+            }
+        board.push(row);
+    }
+};
