8e7c4516df87a0808e9ae80b07085c6ef5bb550b
AlphaWolf7335 <joni.decoster@student.arteveldehs.be>
2024-04-18 15:16:17 +0200

    uibreiding .js

--- connect4.js
+++ connect4.js
@@ -1,3 +1,18 @@
 const player1 = "M";
 const player2 = "P";
-const currPlayer = player1
+const currPlayer = player1;
+
+const gameOver = false; 
+let board; 
+
+const rows = 6;
+const columns = 7;
+const currColums = [];
+
+window.onload = function() {
+    setGame();
+}
+
+function setGame() {
+    board = [];
+    currColums = [5,5,5,5]
